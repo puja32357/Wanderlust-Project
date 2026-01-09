f3a7364f38486e5425084d58cdb7666f95115e92
Puja Kotkar <pujakotkar32@gmail.com>
2026-01-10 00:15:06 +0530

    fix:resolve duplicate variables and syntax errors

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Joi=require("joi");
 
 module.exports.listingSchema=Joi.object({
@@ -17,24 +17,4 @@ module.exports.reviewSchema=Joi.object({
         rating:Joi.number().required().min(1).max(5),
         comment:Joi.string().required(),
     }).required(),
-=======
-const Joi=require("joi");
-
-module.exports.listingSchema=Joi.object({
-    listing:Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        location:Joi.string().required(),
-        country:Joi.string().required(),
-        price:Joi.number().required().min(0),
-        image:Joi.string().allow("" , null),
-    }).required(),
-});
-
-module.exports.reviewSchema=Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        comment:Joi.string().required(),
-    }).required(),
->>>>>>> 52437c7729790020c72bf236de6ae50060ce28c5
 });
